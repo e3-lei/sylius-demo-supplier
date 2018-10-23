f235289403b2a20a9210c993656a5380a7369ed9
e3-lei <e3leihro@gmail.com>
2018-10-23 11:55:10 +0200

    add supplier interface

--- src/AppBundle/Entity/Supplier.php
+++ src/AppBundle/Entity/Supplier.php
@@ -8,9 +8,7 @@ declare(strict_types=1);
 
 namespace AppBundle\Entity;
 
-use Sylius\Component\Resource\Model\ResourceInterface;
-
-class Supplier implements ResourceInterface
+class Supplier implements SupplierInterface
 {
     private $id;
 
